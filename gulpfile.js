0ab1fa2217c6aedf8c2c249d1c38ca433f19ccc0
ATR <ch.tremolieres@gmail.com>
2017-09-28 15:46:57 +0200

    Ajout nouvelles librairies jQuery / Bootstrap / Angular / Fluxy / configuration gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,21 @@
 var gulp = require('gulp');
+var jqConfig = require("jQuery");
+var bsConfig = require("bootstrap");
+
+gulp.task('gulp-bootstrap', function(){
+	console.log('test');
+	/*
+  return gulp.src("./config.json")
+    .pipe(bsConfig.js({
+    	compress: true,
+        name: 'bootstrapGulp.js'
+    }))
+    .pipe(gulp.dest("./assets"));
+    // It will create `bootstrap.js` in directory `assets`. 
+    */
+});
+/*
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+});
+*/
